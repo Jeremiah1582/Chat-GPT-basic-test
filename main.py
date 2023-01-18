f1ca352618ecc932fb00c10a0bedaa92574f4786
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-01-18 21:16:25 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import openai 
+from api_secrets import API_KEY
+
+openai.api_key = API_KEY
+
+prompt = "how many days are in a year"
+
+response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_tokens=6)
+
+print(response)
